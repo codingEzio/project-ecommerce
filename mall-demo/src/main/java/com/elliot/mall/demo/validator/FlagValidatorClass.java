3cba3c8cd2d55ba4dafb2682cb35085f660d2323
!Mensch <assassinste@gmail.com>
2023-03-27 18:51:30 +0800

    feat: Classes that were served like enum/struct for DB fields

---
+++ mall-demo/src/main/java/com/elliot/mall/demo/validator/FlagValidatorClass.java
@@ -0,0 +1,40 @@
+package com.elliot.mall.demo.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * This is a Java class named FlagValidatorClass that implements the
+ * ConstraintValidator interface from the validation API in Java. The
+ * ConstraintValidator interface is used to implement custom validation logic
+ * for a custom validation annotation, in this case the @FlagValidator
+ * annotation.
+ * It kinda looks like enum validation, but with custom logic. I might just
+ * switch this to it in the future.
+ */
+public class FlagValidatorClass implements ConstraintValidator<FlagValidator, Integer> {
+	private String[] values;
+
+	@Override
+	public void initialize(FlagValidator constraintAnnotation) {
+		this.values = constraintAnnotation.value();
+	}
+
+	@Override
+	public boolean isValid(Integer value, ConstraintValidatorContext context) {
+		boolean isValid = false;
+
+		if (value == null) {
+			return true;
+		}
+
+		for (String s : values) {
+			if (s.equals(String.valueOf(value))) {
+				isValid = true;
+				break;
+			}
+		}
+
+		return isValid;
+	}
+}
