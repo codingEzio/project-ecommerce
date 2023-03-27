6765ffc16c259558de805edf937157e5f32087d2
!Mensch <assassinste@gmail.com>
2023-03-27 16:42:41 +0800

    dev: Placeholder for Spring Task

---
+++ mall-portal/src/main/java/com/elliot/mall/portal/config/SpringTaskConfig.java
@@ -0,0 +1,9 @@
+package com.elliot.mall.portal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
